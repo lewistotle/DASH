727745978b38bb14e80560985074b825aa38e033
John Lewis <lewistotle@gmail.com>
2009-02-23 22:48:11 +0000

    Made the default behavior exclusive and added some macros to force immediate transitions into substates if needed.
    git-svn-id: http://dolphinage.com/svn/DASH/trunk@3 7cbd89a2-0507-4f57-bfba-8c646f04f5a3

--- stateMachine.h
+++ stateMachine.h
@@ -1,9 +1,6 @@
 #ifndef _STATE_MACHINE_H_
 #define _STATE_MACHINE_H_
 
-#warning ************************** Put this into a proper header file **************************
-void addTickCounter(	unsigned short* pointerToIncrement) ;
-
 // A couple of states that all state machines MUST have. Others will be defined in the specific
 // instance of the state machine. The first two enums in each state machine must be the two below
 
@@ -23,83 +20,170 @@ enum {
 // Normally putting a variable is a header file isn't done, but it's ok in this case
 // since each state machine will have it's own source file.
 
-static xdata unsigned short	millisecondsInState ;
-static xdata unsigned char	previousState ;
-static xdata unsigned char	currentState ;
-static xdata unsigned char	nextState ;
+static unsigned char	previousState ;
+static unsigned char	currentState ;
+static unsigned char	nextState ;
 
-#if STATE_MACHINE_DEBUGGING_ENABLED
+
+
+
+#if configSTATE_MACHINE_DEBUGGING_ENABLED
 	void outputStateMachineDebugData(	unsigned char subState) ;
 #else
 	#define outputStateMachineDebugData(a)
 #endif
 
 
-#define STATE_MACHINE_INIT()			previousState	= STATE_UNKNOWN ;		\
-										currentState	= STATE_MACHINE_INIT ;	\
-										nextState		= STATE_MACHINE_INIT ;	\
-										addTickCounter(&millisecondsInState) ;
 
 
-#define STATE_RESET_TIMEOUT_COUNTER()	millisecondsInState = 0
+#if configSTATE_MACHINE_TIMEOUTS_ENABLED
+	#if		configSTATE_MACHINE_USE_SHORT_FOR_MILLISECOND_TIMER
+		typedef unsigned short	millisecondTimerType ;
+	#elif	configSTATE_MACHINE_USE_LONG_FOR_MILLISECOND_TIMER
+		typedef unsigned long	millisecondTimerType ;
+	#else
+		#error No type given for millisecondInState variable
+	#endif
 
-#define STATE_CHANGE_CLEANUP()			if(previousState != currentState) { STATE_RESET_TIMEOUT_COUNTER() ; }
+	static millisecondTimerType	millisecondsInState ;
+
+	#define STATE_MACHINE_TIME_IN_STATE_ms		millisecondsInState
+
+	#define STATE_RESET_TIMEOUT_COUNTER()		millisecondsInState = 0
+
+	#define STATE_INCREMENT_TIMEOUT_COUNTER()	millisecondsInState++ ;
+
+	#if configSTATE_MACHINE_TIME_IN_STATE_VARIABLE_BASED
+
+		#ifdef configSTATE_MACHINE_TIME_IN_STATE_VARIABLE_REGISTRY_FUNCTION
+			#define STATE_MACHINE_SETUP_MILLISECOND_TICK	configSTATE_MACHINE_TIME_IN_STATE_VARIABLE_REGISTRY_FUNCTION(&millisecondsInState)
+		#else
+			#error State machine configured to use variable registry function, but none defined
+		#endif
+
+	#elif configSTATE_MACHINE_TIME_IN_STATE_CALLBACK_BASED
+
+		static void incrementMillisecondsInState(	void)
+		{
+			millisecondsInState++ ;
+		}
+
+		#ifdef configSTATE_MACHINE_TIME_IN_STATE_CALLBACK_REGISTRY_FUNCTION
+			#define STATE_MACHINE_SETUP_MILLISECOND_TICK	configSTATE_MACHINE_TIME_IN_STATE_CALLBACK_REGISTRY_FUNCTION(incrementMillisecondsInState)
+		#else
+			#error State machine configured to use increment callback function, but none defined
+		#endif
+
+	#else
+
+		#error State machine timeouts are enabled but no mechanism to determine time has been enabled.
+
+	#endif
+#else
+	#define STATE_RESET_TIMEOUT_COUNTER()
+
+	#define STATE_INCREMENT_TIMEOUT_COUNTER()
+
+	#define STATE_MACHINE_SETUP_MILLISECOND_TICK
+#endif
 
 
 
 
-#define STATE_ENTRY_ACTION				if(currentState != previousState)					\
-										{													\
-											outputStateMachineDebugData(SUBSTATE_ENTRY) ;	\
+#define STATE_MACHINE_INIT()					previousState	= STATE_UNKNOWN ;		\
+												currentState	= STATE_MACHINE_INIT ;	\
+												nextState		= STATE_MACHINE_INIT ;	\
+												STATE_MACHINE_SETUP_MILLISECOND_TICK ;
 
-											// app code goes here
 
-#define STATE_DO_ACTION						previousState = currentState ;					\
-										}													\
-										else if(nextState == currentState)					\
-										{													\
-											outputStateMachineDebugData(SUBSTATE_DO) ;		\
+#define STATE_CHANGE_CLEANUP()					if(previousState != currentState) { STATE_RESET_TIMEOUT_COUNTER() ; }
 
-											// app code goes here
 
-#define STATE_TIMEOUT_ACTION_ms(to)		}													\
-										if(millisecondsInState >= to)						\
-										{													\
-											STATE_RESET_TIMEOUT_COUNTER() ;					\
-											outputStateMachineDebugData(SUBSTATE_TIMEOUT) ;	\
 
-											// app code goes here
 
-#define STATE_EXIT_ACTION				}													\
-										else if(nextState != currentState)					\
-										{													\
-											outputStateMachineDebugData(SUBSTATE_EXIT) ;
+#define STATE_ENTRY_ACTION						if(currentState != previousState)					\
+												{													\
+													outputStateMachineDebugData(SUBSTATE_ENTRY) ;	\
 
-											// app code goes here
+													// app code goes here
 
-#define STATE_END							currentState = nextState ;						\
-										}
+#define STATE_DO_ACTION_EXCLUSIVE					previousState = currentState ;					\
+												}													\
+												else if(nextState == currentState)					\
+												{													\
+													outputStateMachineDebugData(SUBSTATE_DO) ;		\
 
-static void iterateStateMachine(		void) ;
+													// app code goes here
 
-#define ITERATE_STATE_MACHINE()			iterateStateMachine() ;
+#if configSTATE_MACHINE_TIMEOUTS_ENABLED
+	#define STATE_TIMEOUT_ACTION_ms(to)				}													\
+													if(millisecondsInState >= to)						\
+													{													\
+														STATE_RESET_TIMEOUT_COUNTER() ;					\
+														outputStateMachineDebugData(SUBSTATE_TIMEOUT) ;	\
 
-#define STATE_MACHINE_ITERATOR()		void iterateStateMachine(	void)
+														// app code goes here
+#endif
 
-#define STATE_MACHINE_SWITCH()			STATE_CHANGE_CLEANUP() ; switch(currentState)
+#define STATE_EXIT_ACTION_EXCLUSIVE				}													\
+												else if(nextState != currentState)					\
+												{													\
+													outputStateMachineDebugData(SUBSTATE_EXIT) ;
 
-#define DEFAULT_STATE_MACHINE_CASE		default:											\
-										{													\
-											previousState	= STATE_UNKNOWN ;				\
-											currentState	= STATE_MACHINE_INIT ;			\
-											nextState		= STATE_MACHINE_INIT ;			\
-										}
+													// app code goes here
 
-#define STATE_MACHINE_HANDLER(state)	handler_##state()
+#define STATE_END									currentState = nextState ;						\
+												}
+
+
+
+
+
+#define STATE_DO_ACTION_IMMEDIATE					previousState = currentState ;					\
+												}													\
+												if(nextState == currentState)						\
+												{													\
+													outputStateMachineDebugData(SUBSTATE_DO) ;		\
+
+#define STATE_EXIT_ACTION_IMMEDIATE				}													\
+												if(nextState != currentState)						\
+												{													\
+													outputStateMachineDebugData(SUBSTATE_EXIT) ;
+
+
+#if configSTATE_MACHINE_ACTIONS_ARE_IMMEDIATE_BY_DEFAULT
+	#define STATE_DO_ACTION						STATE_DO_ACTION_IMMEDIATE
+	#define STATE_EXIT_ACTION					STATE_EXIT_ACTION_IMMEDIATE
+#else
+	#define STATE_DO_ACTION						STATE_DO_ACTION_EXCLUSIVE
+	#define STATE_EXIT_ACTION					STATE_EXIT_ACTION_EXCLUSIVE
+#endif
+
+
+
+
+
+#define ITERATE_STATE_MACHINE()					iterateStateMachine() ;
+
+#define STATE_MACHINE_ITERATOR()				static void iterateStateMachine(	void)
+
+// declare prototype
+STATE_MACHINE_ITERATOR() ;
+
+#define STATE_MACHINE_SWITCH()					STATE_CHANGE_CLEANUP() ; switch(currentState)
+
+#define DEFAULT_STATE_MACHINE_CASE				default:											\
+												{													\
+													previousState	= STATE_UNKNOWN ;				\
+													currentState	= STATE_MACHINE_INIT ;			\
+													nextState		= STATE_MACHINE_INIT ;			\
+												}
+
+#define STATE_MACHINE_HANDLER(state)			handler_##state()
 
 #define STATE_MACHINE_HANDLER_FUNCTION(state)	static void handler_##state(	void)
 
-#define CHANGE_STATE_TO(newState)		nextState = newState
+#define CHANGE_STATE_TO(newState)				nextState = newState
 
 
 #define STATE_MACHINE_GET_PREVIOUS_STATE()		previousState
