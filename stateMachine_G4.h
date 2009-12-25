6ec1903a382e758098ff7f5fff5e3466445d23ff
John Lewis <lewistotle@gmail.com>
2009-12-25 09:48:39 +0000

    Cleaned up the interface a bit so that it reads really nicely in the source. The header file is a bit uglier, though, and will require further documentation.
    git-svn-id: http://dolphinage.com/svn/DASH/trunk@31 7cbd89a2-0507-4f57-bfba-8c646f04f5a3

--- stateMachine_G4.h
+++ stateMachine_G4.h
@@ -16,17 +16,23 @@ typedef void	(* call_state_type)(uint8_t stateEvent_t) ;
 
 typedef uint16_t	millisecondTimerType ;
 
+enum STATE_HANDLER_RESPONSES { IGNORED, HANDLED, TRANSITION } ;
+
+typedef uint8_t	stateHandlerResponse_t ;
+
 
 typedef struct
 {
 	void*					parent ;
 	uint8_t					type ;
+	void*					handler ;
 } state_t ;
 
 typedef struct
 {
 	void*					parent ;
 	uint8_t					type ;
+	void*					handler ;
 	state_t*				regions ;
 } orthogonalRegion_t ;
 
@@ -53,35 +59,51 @@ typedef struct
 	char*					currentStateName ;
 } stateMachine_t ;
 
+typedef stateHandlerResponse_t (* callStateHandler_t)(stateMachine_t* self, event_t* event) ;
+
+
 
 #define STATE_MACHINE_TOP(sm)					sm##_TOP
 
-#define DECLARE_STATE_MACHINE(sm)				stateMachine_t* createInstanceOf_##sm ;							\
-												void sm##_handleEvent(stateMachine_t* self, event_t* event) ;
+#define DECLARE_STATE_MACHINE(sm)				stateMachine_t* createInstanceOf_##sm ;
 
 #define DECLARE_STATE_MACHINE_EVENTS(sm)		enum sm##_EVENTS { REQUIRED_STATE_MACHINE_EVENTS,
 
 #define END_STATE_MACHINE_EVENTS()				} ;
 
 
-#define DECLARE_STATE_MACHINE_VARIABLES(sm)		typedef struct {
+#define DECLARE_STATE_MACHINE_VARIABLES()		typedef struct {
 	#define PARENT_CLASS(thing)						thing	parent
-#define END_STATE_MACHINE_VARIABLES(sm)			} sm##Machine_t ;
+#define END_STATE_MACHINE_VARIABLES_2(sm)		} sm##Machine_t ;
+#define END_STATE_MACHINE_VARIABLES_1(sm)		END_STATE_MACHINE_VARIABLES_2(sm)
+#define END_STATE_MACHINE_VARIABLES()			END_STATE_MACHINE_VARIABLES_1(STATE_MACHINE_NAME)
 
-#define DEFINE_STATE_MACHINE(sm)				state_t sm##_TOP				= { (void*)0, 0 } ;
+#define DEFINE_STATE_MACHINE_2(sm)				state_t sm##_TOP				= { (void*)0, 0 }
+#define DEFINE_STATE_MACHINE_1(sm)				DEFINE_STATE_MACHINE_2(sm)
+#define DEFINE_STATE_MACHINE()					DEFINE_STATE_MACHINE_1(STATE_MACHINE_NAME)
 
-#define ADD_SUB_STATE(sm, ps, ss)				state_t sm##_##ss				= { &sm##_##ps, 0 } ;
+#define ADD_SUB_STATE_2(sm, ps, ss)				static stateHandlerResponse_t sm##_##ss##_handler(	sm##Machine_t* self, event_t* event) ;	\
+												state_t sm##_##ss				= { &sm##_##ps, 0, &sm##_##ss##_handler }
+#define ADD_SUB_STATE_1(sm, ps, ss)				ADD_SUB_STATE_2(sm, ps, ss)
+#define ADD_SUB_STATE(ps, ss)					ADD_SUB_STATE_1(STATE_MACHINE_NAME, ps, ss)
 
-#define ADD_ORTHOGONAL_REGION(sm, ps, or)		orthogonalRegion_t sm##_##or	= { &sm##_##ps, 0 } ;
+#define ADD_ORTHOGONAL_REGION(sm, ps, or)		orthogonalRegion_t sm##_##or	= { &sm##_##ps, 0 }
 
-#define END_STATE_MACHINE_DEFINITION(sm)
+#define END_STATE_MACHINE_DEFINITION()
 
 stateMachine_t* allocateStateMachineMemory(		uint16_t sizeInBytes) ;
 void deallocateStateMachineMemory(				stateMachine_t* instance) ;
 
-#define CREATE_STATE_MACHINE_INSTANCE(sm)		stateMachine_t* init_##sm(	void)
+#define CREATE_STATE_MACHINE_INSTANCE_2(sm)		stateMachine_t* create_##sm(	void)
+#define CREATE_STATE_MACHINE_INSTANCE_1(sm)		CREATE_STATE_MACHINE_INSTANCE_2(sm)
+#define CREATE_STATE_MACHINE_INSTANCE()			CREATE_STATE_MACHINE_INSTANCE_1(STATE_MACHINE_NAME)
 
-#define DESTROY_STATE_MACHINE_INSTANCE(sm)		void destroy_##sm(			stateMachine_t* instance)
+#define DESTROY_STATE_MACHINE_INSTANCE_2(sm)	void destroy_##sm(			stateMachine_t* instance)
+#define DESTROY_STATE_MACHINE_INSTANCE_1(sm)	DESTROY_STATE_MACHINE_INSTANCE_2(sm)
+#define DESTROY_STATE_MACHINE_INSTANCE()		DESTROY_STATE_MACHINE_INSTANCE_1(STATE_MACHINE_NAME)
+
+#define CREATE_INSTANCE_OF(sm)					stateMachine_t* create_##sm(	void)
+#define DESTROY_INSTANCE_OF(sm)					void destroy_##sm(				stateMachine_t*)
 
 #define REQUIRED_STATE_MACHINE_EVENTS			SUBSTATE_GET_INFO,										\
 												SUBSTATE_ENTRY,											\
@@ -89,41 +111,92 @@ void deallocateStateMachineMemory(				stateMachine_t* instance) ;
 												SUBSTATE_EXIT
 
 
-#define DEFINE_TOP_STATE(sm)					state_t* sm##_TOP_handler(			sm##Machine_t* self, event_t* event)
-#define DEFINE_STATE(sm, state)					state_t* sm##_##state##_handler(	sm##Machine_t* self, event_t* event)
+#define DEFINE_TOP_STATE_2(sm)					static stateHandlerResponse_t sm##_TOP_handler(			sm##Machine_t* self, event_t* event)
+#define DEFINE_TOP_STATE_1(sm)					DEFINE_TOP_STATE_2(sm)
+#define DEFINE_TOP_STATE()						DEFINE_TOP_STATE_1(STATE_MACHINE_NAME)
+#define DEFINE_STATE_2(sm, state)				static stateHandlerResponse_t sm##_##state##_handler(	sm##Machine_t* self, event_t* event)
+#define DEFINE_STATE_1(sm, state)				DEFINE_STATE_2(sm, state)
+#define DEFINE_STATE(state)						DEFINE_STATE_1(STATE_MACHINE_NAME, state)
 
 #define HANDLE_STATE_EVENTS						switch(event->eventType)								\
 												{
 
 #define ENTER										case SUBSTATE_ENTRY:								\
 													{
-#define ENTER_DONE										return 0 ;										\
+#define ENTER_DONE										return HANDLED ;								\
 													}
 
-#define INITIAL_TRANSITION							case SUBSTATE_INITIAL_TRANSITION:					\
+#define INITIAL_TRANSITION_EVENT					case SUBSTATE_INITIAL_TRANSITION:					\
 													{
-#define INITIAL_TRANSITION_DONE							return 0 ;										\
+#define INITIAL_TRANSITION_DONE							return HANDLED ;								\
 													}
 
 #define EXIT										case SUBSTATE_EXIT:									\
 													{
-#define EXIT_DONE										return 0 ;										\
+#define EXIT_DONE										return HANDLED ;								\
 													}
 
 #define EVENT(evt)									case evt:											\
 													{
-#define EVENT_DONE										return 0 ;										\
+#define EVENT_DONE										return HANDLED ;								\
 													}
 
 
 #define HANDLE_STATE_EVENTS_DONE					default:											\
 													{													\
-														return (state_t*)(((stateMachine_t*)self)->currentState->parent) ;	\
+														return IGNORED ;								\
 													}													\
 												}
 
 
-#define CHANGE_STATE_TO(st)
+#define CHANGE_STATE_TO(sm, st)					((stateMachine_t*)self)->nextState = &sm##_##st ; return HANDLED ;
+
+
+
+#define INITIAL_TRANSITION(dest, act)			if(event->eventType == SUBSTATE_INITIAL_TRANSITION) { act ; dest ; return TRANSITION ; }
+#define TRANSITION_ON(evt, cndtn, dest, act)	if((event->eventType == evt) && (cndtn)) { act ; dest ; return TRANSITION ; }
+#define IF(cndtn)								(cndtn)
+#define UNCONDITIONALLY							true
+#define NO_ACTION
+#define ACTION(act)								act
+#define FINISH									((stateMachine_t*)self)->nextState = 0
+
+#define TO_2(sm, st)							((stateMachine_t*)self)->nextState = &sm##_##st
+#define TO_1(sm, st)							TO_2(sm, st)
+#define TO(st)									TO_1(STATE_MACHINE_NAME, st)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
